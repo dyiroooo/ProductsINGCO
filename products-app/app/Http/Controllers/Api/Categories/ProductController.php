815bab3b707eae9a2d19a24fff410e2c5e35af33
Steven <johnsteven.ingcoph@gmail.com>
2023-06-26 15:48:18 +0800

    Back End Complete

--- products-app/app/Http/Controllers/Api/Categories/ProductController.php
+++ products-app/app/Http/Controllers/Api/Categories/ProductController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api\Categories;
 
+
 use App\Models\Products;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Api\Categories\ProductController;
 
 class ProductController extends Controller
 {
@@ -30,12 +32,11 @@ class ProductController extends Controller
     }
 
     public function create(Request $request) {
-        $product = Products::create();
 
         $validator = Validator::make($request->all(), [
             'product_name' => 'required|string|max:20',
             'product_type' => 'required|string|max:20',
-            'product_description' => 'string|max:16000'
+            'product_description' => 'required|string|max:16000'
         ]);
 
         if($validator->fails()) {
@@ -43,6 +44,7 @@ class ProductController extends Controller
                 'status' => 422,
                 'products' => $validator->messages()
             ], 422);
+            
         } else {
 
             $product = Products::create([
@@ -68,20 +70,103 @@ class ProductController extends Controller
     }
 
     public function getbyId($id) {
-        $product = Product::find($id);
+
+        $product = Products::find($id);
 
         if($product) {
             return response()->json([
                 'status' => 200,
-                'products' => $product
+                'product' => $product
             ], 200);
         } else {
             return response()->json([
-                'status' => 500,
-                'products' => $product
-            ], 500);
+                'status' => 404,
+                'product' => "Selected Student Not Found"
+            ], 404);
         }
     }
+
+    public function edit($id) {
+        $product = Products::find($id);
+
+        if($product) {
+            return response()->json([
+                'status' => 200,
+                'product' => $product
+            ], 200);
+
+        } else {
+            return response()->json([
+                'status' => 404,
+                'product' => "Selected Student Not Found"
+            ], 404);
+        }
+    }
+
+    public function update(Request $request, int $id) {
+
+        $validator = Validator::make($request->all(), [
+            'product_name' => 'required|string|max:20',
+            'product_type' => 'required|string|max:20',
+            'product_description' => 'required|string|max:16000'
+        ]);
+
+        if($validator->fails()) {
+            return response() -> json([
+                'status' => 422,
+                'products' => $validator->messages()
+            ], 422);
+            
+        } else {
+
+            $product = Products::find($id);
+
+            if ($product) {
+
+                $product->update([
+                    'product_name' => $request->product_name,
+                    'product_type' => $request->product_type,
+                    'product_description' => $request->product_description
+                ]);
+
+                $updatemsgs = "Product Updated Successfully!";
+                return response()->json([
+                    'status' => 200,
+                    'products' => $updatemsgs
+                ], 200);
+            } else {
+                $notcreatedmsg = "Product NOT Updated Successfully!";
+                return response()->json([
+                    'status' => 500,
+                    'products' => $notcreatedmsg
+                ], 500);
+            }
+        }
+    }
+
+    public function delete($id) {
+
+        $product = Products::find($id);
+
+        if ($product) {
+
+            $product->delete([]);
+            
+            $updatemsgs = "Product Deleted!";
+            return response()->json([
+                'status' => 200,
+                'products' => $updatemsgs
+            ], 200);
+        } else {
+            $notfound = "Student not Found!";
+            return response()->json([
+                'status' => 404,
+                'products' => $notfound
+            ], 404);
+        }
+    }
+
+
 
 
 }
--- products-app/routes/api.php
+++ products-app/routes/api.php
@@ -9,3 +9,9 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'create']);
 
 Route::get('/products/{id}', [ProductController::class, 'getbyId']);
+
+Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
+
+Route::put('/products/{id}/edit', [ProductController::class, 'update']);
+
+Route::delete('/products/{id}/delete', [ProductController::class, 'delete']);
